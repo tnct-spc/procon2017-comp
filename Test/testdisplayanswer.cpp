ab28ce1e9327131b5ce0933f1a9a16eca5b38e7b
ishowta <kinironote@gmail.com>
2016-06-22 19:01:00 +0900

    新しいfieldに対応。

--- Test/testdisplayanswer.cpp
+++ Test/testdisplayanswer.cpp
@@ -11,16 +11,16 @@ bool TestDisplayAnswer::run()
     std::cout << "Hello,display!" << std::endl;
     polygon_t b,c,d;
     bg::exterior_ring(b) = boost::assign::list_of<point_t>(0, 0)(30, 0)(30,30)(0, 30)(0, 2)(2, 2)(2,28)(28, 28)(28, 2)(0, 2)(0,0);
-    bg::exterior_ring(c) = boost::assign::list_of<point_t>(0, 0)(0,3)(3,3)(3,0)(0,0);
-    Field field;
-    field.setFlame(b);
-    field.pushPiece(c);
-    field.pushPiece(c);
-    field.pushPiece(c);
-    d = field.popPiece();
-    std::cout << bg::dsv(d) << std::endl;
-    field.printFlame();
-    field.printPiece();
-    disp->setField(field);
+    bg::exterior_ring(c) = boost::assign::list_of<point_t>(0, 0)(0,1)(1,0)(0,0);
+    Field f;
+    PolygonExpansion hoge,huga;
+    hoge.setPolygon(b);
+    huga.setPolygon(c);
+    f.setFlame(hoge);
+    f.pushPiece(huga);
+    f.setPiece(huga,1919);
+    f.printFlame();
+    f.printPiece();
+    disp->setField(f);
     return true;
 }
