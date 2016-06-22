7520d7f26bce18bdabd92d8a956189f07754761a
in9b <asayuuya.m9@gmail.com>
2016-06-23 03:33:51 +0900

    相対パス指定にした & dpiの設定の追加 & 実行時間の計測の追加

--- scanimage.sh
+++ scanimage.sh
@@ -4,18 +4,28 @@
 #スキャナより画像を読み込むShellScript
 #
 
+#sh scanimage.sh
+#上のコマンドで実行
 
 ##===ドライバのインストール方法===###
 #https://wiki.archlinuxjp.org/index.php/Sane
 #上のページを参考にインストールする。
 
 ##以下自機で実行したコマンド
+#ただしMFC-6890CN(プリンタの型番)用
 # sudo pacman -S sane
 # yaourt -S brscan3
 # /usr/local/Brother/sane/setupSaneScan3 -i
 
 
-#任意のディレクトリに保存
-#相対パスの指定の仕方がよくわからないので、/home直下に
+#このスクリプトが置かれている場所/image下(./image/)にファイルを保存する
+#$script_dir_pathが./に相当する
+#SECONDSは時間計測用．BashScriptが起動している間は毎秒1ずつ増える変数
 
-scanimage --format=png > ~/test.png
+script_dir_path=$(dirname $(readlink -f $0))
+SECONDS=0
+scanimage --format=png --resolution 300 > $script_dir_path/image/dpi300test.png
+echo time=$SECONDS
+SECONDS=0
+scanimage --format=png --resolution 600 > $script_dir_path/image/dpi600test.png
+echo time=$SECONDS
