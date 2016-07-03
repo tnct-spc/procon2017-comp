8f9fcb9f5f8b8390104387545fce29519aae6a75
ishowta <kinironote@gmail.com>
2016-07-03 19:48:47 +0900

    フィールドをセットしないとnull参照してしまうバグを修正

--- DisplayAnswer/displayanswer.cpp
+++ DisplayAnswer/displayanswer.cpp
@@ -3,11 +3,12 @@
 
 DisplayAnswer::DisplayAnswer()
 {
-    board.showMaximized();
 }
 
 void DisplayAnswer::setField(Field &field)
 {
+    board = std::make_shared<AnswerBoard>();
+    board->showMaximized();
     this->field = field;
-    board.setField(this->field);
+    board->setField(this->field);
 }
