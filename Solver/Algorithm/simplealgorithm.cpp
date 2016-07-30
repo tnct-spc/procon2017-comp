c39e33d238a3cfb6986de231e49ea4a9f9401764
ishowta <kinironote@gmail.com>
2016-07-30 17:35:10 +0900

    simpleAlgorithmの実装

--- Solver/Algorithm/simplealgorithm.cpp
+++ Solver/Algorithm/simplealgorithm.cpp
@@ -8,5 +8,26 @@ SimpleAlgorithm::SimpleAlgorithm()
 Field SimpleAlgorithm::run(PolygonSet set)
 {
     Field field;
+    PolygonExpansion ExP_buff;
+    polygon_t P_buff;
+
+    field.setFlame(set.fieldFlame);
+    int size = set.fieldPiece.size();
+    for(int i=0;i<size;i++){
+        for(double x=0;x<30;x++){
+            for(double y=0;y<30;y++){
+                boost::geometry::strategy::transform::translate_transformer<double,2,2> translate(x,y);
+                boost::geometry::transform(set.fieldPiece.at(i).getPolygon(),P_buff,translate);
+                ExP_buff = set.fieldPiece.at(i);
+                ExP_buff.setPolygon(P_buff);
+                if(field.isPuttable(ExP_buff)){
+                    field.pushPiece(ExP_buff);
+                    goto next;
+                }
+            }
+        }
+        next:;
+    }
+
     return field;
 }
