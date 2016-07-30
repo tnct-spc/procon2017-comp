190a7f534424182abc90f7bfcc5e038935d7c648
ishowta <kinironote@gmail.com>
2016-07-30 18:37:31 +0900

    DisplayAnswerに色をつけた

--- DisplayAnswer/answerboard.cpp
+++ DisplayAnswer/answerboard.cpp
@@ -38,22 +38,48 @@ void AnswerBoard::paintEvent(QPaintEvent *)
         painter.drawLine((x1*x/max)+x_margin, (y1*y/max)+y_margin, (x2*x/max)+x_margin, (y2*y/max)+y_margin);
     };
 
-    //drawField
-    polygon_t raw_flame = field.getFlame().getPolygon();
+    auto drawPolygon = [&](std::vector<QPointF> points){
+        int size = points.size();
+        QPointF* draw_point = new QPointF[points.size()];
+        static const int max = 30;
+        static const int margin = 20;
+        int y = height - margin;
+        int x = height - margin;
+        int y_margin = margin/2;
+        int x_margin = (width-height)/2 + margin/2;
+        for(int i=0;i<size;i++){
+            draw_point[i].setX(((points.at(i).x())*x/max)+x_margin);
+            draw_point[i].setY(((points.at(i).y())*y/max)+y_margin);
+        }
+        painter.drawPolygon(draw_point,size);
+        delete[] draw_point;
+    };
+
+    //Field
+    painter.setBrush(QBrush(QColor("#d4c91f")));
+    painter.drawRect(0,0,width,height);
+
     //flame
+    polygon_t raw_flame = field.getFlame().getPolygon();
     int flame_size = raw_flame.outer().size();
+    std::vector<QPointF> points;
     for(int i=0;i<flame_size;i++){
-        drawLine(raw_flame.outer()[i].x(), raw_flame.outer()[i].y(), raw_flame.outer()[(i+1)%flame_size].x(), raw_flame.outer()[(i+1)%flame_size].y());
+        points.push_back(QPointF(raw_flame.outer()[i].x(),raw_flame.outer()[i].y()));
     }
+    painter.setBrush(QBrush(QColor("#d0b98d")));
+    drawPolygon(points);
+
     //piece
     std::vector<polygon_t> raw_pieces;
     int pieces_size = field.pieceSize();
     for(int i=0;i<pieces_size;i++){
-        raw_pieces.push_back(field.getPiece(i).getPolygon());
-        int piece_size = raw_pieces[i].outer().size()-1;
+        int piece_size = field.getPiece(i).getPolygon().outer().size()-1;
+        std::vector<QPointF> points;
         for(int j=0;j<piece_size;j++){
-            drawLine(raw_pieces[i].outer()[j].x(), raw_pieces[i].outer()[j].y(), raw_pieces[i].outer()[(j+1)%flame_size].x(), raw_pieces[i].outer()[(j+1)%flame_size].y());
+            points.push_back(QPointF(field.getPiece(i).getPolygon().outer()[j].x(),field.getPiece(i).getPolygon().outer()[j].y()));
         }
+        painter.setBrush(QBrush(QColor("#0f5ca0")));
+        drawPolygon(points);
     }
 
 }
