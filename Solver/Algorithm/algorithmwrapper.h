f4bdc8530b35969c07463b0ec37fe2206c6e1808
taki-d <taka410199@outlook.jp>
2016-08-30 14:16:46 +0900

    [add]algorithm wrapperに評価値とpolygonを格納する構造体を定義

--- Solver/Algorithm/algorithmwrapper.h
+++ Solver/Algorithm/algorithmwrapper.h
@@ -8,6 +8,19 @@ class AlgorithmWrapper
 public:
     AlgorithmWrapper();
     virtual procon::Field run(procon::Field field);
+    
+    typedef struct PieceAssesment{
+        //評価値
+        int EvaluationValue;
+        //評価したpolygon
+        procon::ExpandedPolygon Polygon;
+
+        bool operator<(const PieceAssesment& Pieceassesment) const {
+            return EvaluationValue < Pieceassesment.EvaluationValue;
+        }
+
+    } PieceAssesment;
+    
 };
 
 #endif // ALGORITHMWRAPPER_H
