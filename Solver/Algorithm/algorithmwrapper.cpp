31afa95c5d55bca99e7d0ff283b9871ae451a486
reinagaya <rei.nagaya.00@gmail.com>
2016-08-30 22:21:47 +0900

    [update]とりあえず動くと思う

--- Solver/Algorithm/algorithmwrapper.cpp
+++ Solver/Algorithm/algorithmwrapper.cpp
@@ -1,6 +1,8 @@
 #include "algorithmwrapper.h"
 #include <math.h>
 
+#define PI 3.141592
+
 AlgorithmWrapper::AlgorithmWrapper()
 {
 
@@ -11,48 +13,77 @@ procon::Field AlgorithmWrapper::run(procon::Field field)
     return field;
 }
 
-procon::ExpandedPolygon joinPolygon(ExpandedPolygon Polygon1,int Polygon1_start_pos,int Polygon1_end_pos, ExpandedPolygon Polygon2,int Polygon2_start_pos,int Polygon2_end_pos)
-{
-    // heikouidou
-    polygon_t movePolygon;// Polygon2noidouato
-    int point_differentx = Polygon2.getPolygon().outer()[Polygon2_start_pos].x - Polygon1.getPolygon().outer()[Polygon1_start_pos].x;
-    int point_differenty = Polygon2.getPolygon().outer()[Polygon2_start_pos].y - Polygon1.getPolygon().outer()[Polygon1_start_pos].y;
-    int size2 = Polygon2.getSize().outer();// Polygon2nokakusuu
-    for (int i=0; i<size2; i++)// Polygon2nosubetenokakuwoidou
-    {
-        movePolygon.getPolygon().outer()[i].x = Polygon2.getPolygon().outer()[i].x - point_differentx;
-        movePolygon.getPolygon().outer()[i].y = Polygon2.getPolygon().outer()[i].y - point_differenty;
-    }
 
-    // kaitenidou
-    int x1 = movePolygon.getPolygon().outer()[Polygon2_end_pos].x - Polygon1.getPolygon().outer()[Polygon1_start_pos].x;
-    int y1 = movePolygon.getPolygon().outer()[Polygon2_end_pos].y - Polygon1.getPolygon().outer()[Polygon1_start_pos].y;
-    int x2 = Polygon1.getPolygon().outer()[Polygon1_end_pos].x - Polygon1.getPolygon().outer[Polygon1_start_pos].x;
-    int y2 = Polygon1.getPolygon().outer()[Polygon1_end_pos].y - Polygon1.getPolygon().outer[Polygon1_start_pos].y;
-    int cos = (x1*x2 + y1*y2)/(sqrt(x1*x1 + y1*y1)*sqrt(x2*x2 + y2*y2));// idoumaetoidougonokaitennkakudo
-    int sin = sqrt(1 - cos*cos);
-    for (int i=0; i<size2; i++)// Polygon2wokaitennidou
+
+procon::ExpandedPolygon joinPolygon(procon::ExpandedPolygon Polygon1,int Polygon1_start_pos,int Polygon1_end_pos, procon::ExpandedPolygon Polygon2,int Polygon2_start_pos,int Polygon2_end_pos)
+{
+    double x2 = Polygon2.getPolygon().outer()[Polygon2_end_pos].x() - Polygon2.getPolygon().outer()[Polygon2_start_pos].x();
+    double y2 = Polygon2.getPolygon().outer()[Polygon2_end_pos].y() - Polygon2.getPolygon().outer()[Polygon2_start_pos].y();
+    double degree2 = atan2(y2, x2) * 180 / PI;
+
+    double x1 = Polygon1.getPolygon().outer()[Polygon1_end_pos].x() - Polygon1.getPolygon().outer()[Polygon1_start_pos].x();
+    double y1 = Polygon1.getPolygon().outer()[Polygon1_end_pos].y() - Polygon1.getPolygon().outer()[Polygon1_start_pos].y();
+    double degree1 = atan2(y1, x1) * 180 / PI;
+
+    double degree = degree1 - degree2;
+    Polygon2.rotatePolygon(degree);
+
+    double move_x = Polygon2.getPolygon().outer()[Polygon2_start_pos].x() - Polygon1.getPolygon().outer()[Polygon1_start_pos].x();
+    double move_y = Polygon2.getPolygon().outer()[Polygon2_start_pos].y() - Polygon1.getPolygon().outer()[Polygon1_start_pos].y();
+
+    double sumx = 0, sumy = 0;
+    int size2 = Polygon2.getSize();
+
+    for (int i=0; i<size2; i++)
     {
-        if (i != Polygon2_end_pos)
+        sumx = sumx + Polygon2.getPolygon().outer()[i].x() - move_x;
+        sumy = sumy + Polygon2.getPolygon().outer()[i].y() - move_y;
+    }
+    double balance_x = sumx / size2;
+    double balance_y = sumy / size2;
+    Polygon2.setPolygonPosition(balance_x, balance_y);
+
+    polygon_t new_pieces;
+    int count=0;
+    int Type=1;
+    int size1 = Polygon1.getSize();
+    while (count < size1)
+    {
+        double x,y;
+        if (Type == 1)
         {
-            int move_x = movePolygon.getPolygon().outer()[i].x - Polygon1.getPolygon().outer()[Polygon1_start_pos].x;
-            int move_y = movePolygon.getPolygon().outer()[i].y - Polygon1.getPolygon().outer()[Polygon1_start_pos].y;
-            movePolygon.getPolygon().outer()[i].x = move_x * cos - move_y * sin + Polygon1.getPolygon().outer()[Polygon1_start_pos].x;
-            movePolygon.getPolygon().outer()[i].y = move_x * sin + move_y * cos + Polygon1.getPOlygon().outer()[Polygon1_start_pos].y;
+            x = Polygon1.getPolygon().outer()[count].x();
+            y = Polygon1.getPolygon().outer()[count].y();
+            if (count == Polygon1_start_pos)
+            {
+                Type = 2;
+                count = Polygon2_start_pos;
+            }
+            else if (count == Polygon1_end_pos)
+            {
+                Type = 2;
+                count = Polygon2_end_pos;
+            }
         }
+        else if (Type == 2)
+        {
+            x = Polygon2.getPolygon().outer()[count].x();
+            y = Polygon2.getPolygon().outer()[count].y();
+            if (count == Polygon2_start_pos)
+            {
+                Type = 1;
+                count = Polygon1_start_pos;
+            }
+            else if (count == Polygon2_end_pos)
+            {
+                Type = 1;
+                count = Polygon1_end_pos;
+            }
+        }
+        new_pieces.outer().push_back(point_t(x,y));
+        count++;
     }
-    for (int i=0; i<size1; i++)
-    {
-        movePolygon.getPolygon().outer()[size2+i].x = Polygon1.getPolygon().outer()[i].x;
-        movePolygon.getPolygon().outer()[size2+i].y = Polygon1.getPolygon().outer()[i].y;
-    }
-    polygon_t newPolygon;
-    int size1 = Polygon1.getSize().outer();
-
-    return newPolygon;
-}
-
-procon::ExpandedPolygon
-{
-
+    procon::ExpandedPolygon new_polygon;
+    new_polygon.setPolygon(new_pieces);
+    return new_polygon;
 }
