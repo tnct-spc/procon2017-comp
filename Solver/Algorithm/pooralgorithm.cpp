787ad13f4be8537746e653af6d659e7f9ba9be1d
Naoto.S <l_nothing@icloud.com>
2016-08-30 15:53:32 +0900

    [fix change] 探索を全ピースに対して実装、テスト済み

--- Solver/Algorithm/pooralgorithm.cpp
+++ Solver/Algorithm/pooralgorithm.cpp
@@ -8,13 +8,21 @@ PoorAlgorithm::PoorAlgorithm()
 procon::Field PoorAlgorithm::run(procon::Field field)
 {
     int status;
+    int i;
+    int j;
+    int maxpiece;
     std::vector<std::array<Fit,2>> result;
     field.setFlame(field.getElementaryFlame());
     field.setPiece(field.getElementaryPieces().at(0),15.0,15.0);
-    for (int i=1 ; i<21 ; i++)
+    maxpiece = static_cast<int>(field.getElementaryPieces().size());
+    for (i=0 ; i<maxpiece ; i++)
     {
-        status = searchSameLength(field.getElementaryPieces()[0],field.getElementaryPieces()[i], result);
-        std::cout << "status is " << status << "." << std::endl;
+        for (j=0 ; j<maxpiece ; j++)
+        {
+            status = searchSameLength(field.getElementaryPieces()[i],field.getElementaryPieces()[j], result);
+            std::cout << "status is " << status << "." << std::endl;
+            std::cout << "result size is " << result.size() << "." << std::endl;
+        }
     }
     return field;
 }
