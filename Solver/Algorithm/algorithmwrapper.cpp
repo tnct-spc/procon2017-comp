faec93b634ff9129766a60f64df6f99a0f760aef
in9b <asayuuya.m9@gmail.com>
2016-09-12 15:55:23 +0900

    [clean]不要な引数を削除

--- Solver/Algorithm/algorithmwrapper.cpp
+++ Solver/Algorithm/algorithmwrapper.cpp
@@ -13,8 +13,6 @@ procon::Field AlgorithmWrapper::run(procon::Field field)
 int AlgorithmWrapper::searchSameLength(procon::ExpandedPolygon polygon1, procon::ExpandedPolygon polygon2, std::vector<std::array<Fit,2>> &result)
 {
     /*許容誤差指定*/
-    constexpr double length_error = 0.05; // 単位CM
-    constexpr double angle_error = 0.017; //単位rad 0.017rad=1°
     double comped_first = 0;
     double comping_first = 0;
     int Eva = 0;
@@ -31,7 +29,7 @@ int AlgorithmWrapper::searchSameLength(procon::ExpandedPolygon polygon1, procon:
 
                 int start_polygon1 = i;
                 int start_polygon2 = j;
-                Fit::DotORLine dot_or_line = AlgorithmWrapper::findEnd(polygon2, polygon1, start_polygon2, start_polygon1,length_error, angle_error, Eva);
+                Fit::DotORLine dot_or_line = AlgorithmWrapper::findEnd(polygon2, polygon1, start_polygon2, start_polygon1, Eva);
                 fits.at(0).end_dot_or_line = dot_or_line;
                 fits.at(0).end_id=start_polygon1;
                 fits.at(1).end_dot_or_line = dot_or_line;
@@ -50,7 +48,7 @@ int AlgorithmWrapper::searchSameLength(procon::ExpandedPolygon polygon1, procon:
                 if(!isDuplicate){
                     start_polygon1 = i;
                     start_polygon2 = j;
-                    dot_or_line = AlgorithmWrapper::findEnd(polygon1, polygon2, start_polygon1, start_polygon2,length_error, angle_error, Eva);
+                    dot_or_line = AlgorithmWrapper::findEnd(polygon1, polygon2, start_polygon1, start_polygon2, Eva);
                     fits.at(0).start_dot_or_line = dot_or_line;
                     fits.at(0).start_id=start_polygon1;
                     fits.at(1).start_dot_or_line = dot_or_line;
@@ -69,7 +67,7 @@ int AlgorithmWrapper::searchSameLength(procon::ExpandedPolygon polygon1, procon:
     return maxEva;
 }
 
-Fit::DotORLine AlgorithmWrapper::findEnd(procon::ExpandedPolygon polygon1, procon::ExpandedPolygon polygon2,int &comp1,int &comp2, double length_error, double angle_error, int &Eva)
+Fit::DotORLine AlgorithmWrapper::findEnd(procon::ExpandedPolygon polygon1, procon::ExpandedPolygon polygon2,int &comp1,int &comp2, int &Eva)
 {
     double comped;
     double comping;
