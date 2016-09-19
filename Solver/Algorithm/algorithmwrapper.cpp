d148a9a0c09795343c6f12a0fd2a7e8155382ff7
ishowta <kinironote@gmail.com>
2016-09-19 12:03:43 +0900

    [update] samelength評価の実装

--- Solver/Algorithm/algorithmwrapper.cpp
+++ Solver/Algorithm/algorithmwrapper.cpp
@@ -1,5 +1,6 @@
 #include "algorithmwrapper.h"
 #include "utilities.h"
+#include "Evaluation/searchsamelength.h"
 
 #include <math.h>
 
@@ -12,37 +13,21 @@ procon::Field AlgorithmWrapper::run(procon::Field field)
 
 std::vector<Evaluation> AlgorithmWrapper::evaluateCombination(procon::ExpandedPolygon frame, procon::ExpandedPolygon piece)
 {
-    double frame_first = 0;
-    double piece_first = 0;
     std::vector<Evaluation> evaluations;
 
-    for (int k = 0; k < static_cast<int>(frame.getPolygon().inners().size()); ++k){
-        const int inner_size = static_cast<int>(frame.getPolygon().inners().at(k).size() - 1);
-        for (int i = 0; i < inner_size; ++i) {
-            frame_first = frame.getInnersSideAngle().at(k).at(i);
-            for (int j = 0; j < piece.getSize(); ++j) {
-                piece_first = piece.getSideAngle()[j];
+    std::vector<std::array<Fit,2>> fits;
+    std::vector<int> eva_point = SearchSameLength::evaluateMatching(frame, piece, fits);
 
-                if(procon::nearlyEqual(frame_first,piece_first,angle_error)){
-                    std::array<Fit,2> fits;
-                    fits.at(0).start_id = i;
-                    fits.at(0).end_id = i;
-                    fits.at(0).flame_inner_pos = k;
-                    fits.at(0).start_dot_or_line = Fit::Dot;
-                    fits.at(0).end_dot_or_line = Fit::Dot;
-                    fits.at(1).start_id = j;
-                    fits.at(1).end_id = j;
-                    fits.at(1).start_dot_or_line = Fit::Dot;
-                    fits.at(1).end_dot_or_line = Fit::Dot;
-                    Evaluation eva;
-                    eva.frame_id = k;
-                    eva.fits = fits;
-                    eva.evaluation = bg::area(piece.getPolygon());
-                    evaluations.push_back(eva);
-                }
-            }
+    for(int fit_number=0; fit_number < fits.size(); ++fit_number){
+        if(eva_point[fit_number] > 0){
+            Evaluation eva;
+            eva.frame_id = fits[fit_number].at(0).flame_inner_pos;
+            eva.fits = fits[fit_number];
+            eva.evaluation = eva_point[fit_number];
+            evaluations.push_back(eva);
         }
     }
+
     return evaluations;
 }
 
