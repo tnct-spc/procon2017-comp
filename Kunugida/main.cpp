82d6999efda1e759e507e2384ed3c39d5c84d283
taki-d <taka410199@gmail.com>
2017-05-10 22:23:34 +0900

    [add]add spdlog

--- Kunugida/main.cpp
+++ Kunugida/main.cpp
@@ -1,8 +1,11 @@
 #include "kunugida.h"
+#include "spdlog/spdlog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    auto logger = spdlog::stdout_color_mt("Kunugida");
+
     QApplication a(argc, argv);
     Kunugida w;
     w.show();
