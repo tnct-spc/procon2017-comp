96b21f1e84db55280680e590ac1b3fecd8be43d6
taki-d <taka410199@gmail.com>
2017-05-13 12:55:35 +0900

    [add]頂点がint型のPolygonを追加

--- Polygon/neoexpandedpolygon.h
+++ Polygon/neoexpandedpolygon.h
@@ -4,100 +4,31 @@
 #include "fit.h"
 #include <iostream>
 #include <vector>
+#include <boost/geometry/geometries/point.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
 
-namespace procon {
 
-class point {
+namespace bg = boost::geometry;
+typedef bg::model::d2::point_xy<int> point_i;
+typedef bg::model::polygon<point_i> polygon_i;
 
-    // グリッド点のx,yを保存
-    int x;
-    int y;
-
-public:
-    // constractor
-    point(){
-        x = 0;
-        y = 0;
-    }
-
-    point(int x_, int y_) {
-        x = x_;
-        y = y_;
-    }
-};
 
 class NeoExpandedPolygon
 {
-    //メンバ
-    int size;
-    int id;
-    std::vector<point> points;
-    std::vector<point> inner_points;
+    //constractor
+    NeoExpandedPolygon();
 
-    // std::vector<int> multi_ids;
-    std::vector<double> side_length;
-    std::vector<double> side_angle;
-    std::vector<double> side_slope;
-
-    int inners_size;
-    std::vector<std::vector<double>> inners_side_length;
-    std::vector<std::vector<double>> inners_side_angle;
-    std::vector<std::vector<double>> inners_side_slope;
-
-    //flag
-    bool calcSize_flag = false;
+private:
+    polygon_i polygon;
 
 public:
-    //constructor
-    NeoExpandedPolygon();
-    NeoExpandedPolygon(std::vector<point>);
-    NeoExpandedPolygon(int id_ = -1);
-    // NeoExpandedPolygon(std::vector<int> multi_ids_);
-    NeoExpandedPolygon(NeoExpandedPolygon const& p);
-
     //getter
-    int getSize() const;
-    int getInnerSize() const;
-    // std::vector<double> const& getSideLength() const;
-    // std::vector<double> const& getSideAngle() const;
-    // std::vector<double> const& getSideSlope() const;
-    // std::vector<std::vector<double>> const& getInnersSideLength() const;
-    // std::vector<std::vector<double>> const& getInnersSideAngle() const;
-    // std::vector<std::vector<double>> const& getInnersSideSlope() const;
-    int getId() const;
-    // std::vector<int> getMultiIds() const;
-    // std::string makeMultiIdString() const;
-    // std::vector<procon::ExpandedPolygon> const& getJointedPieces() const;
+    polygon_i getPolygon();
 
     //setter
-    // void setMultiIds(std::vector<int> multi_ids_);
-    // void resetPolygonForce(polygon_t const & p);
-    // void pushNewJointedPolygon(polygon_t const & new_frame, procon::ExpandedPolygon const& jointed_polygon);
-    // void replaceJointedPieces(std::vector<procon::ExpandedPolygon> pieces);
+    void setPolygon(polygon_i polygon);
 
-    //operator
-    // ExpandedPolygon operator = (ExpandedPolygon const& p);
-
-    //calcAll
-    // void updatePolygon(bool calc = false);
-
-    void calcSize();
-    // void inversePolygon();
-    // void rotatePolygon(double degree);
-    // void translatePolygon(double x,double y);
-
-    // void setPolygonAngle(double degree);
-    // void setPolygonPosition(double x,double y);
-
-    // void sortJointedPieces();
-
-    // double difference_of_default_degree = 0;
-
-    // double centerx = 0;
-    // double centery = 0;
-
-    // bool is_inverse = false;
 };
-}
 
 #endif // NEOEXPANDEDPOLYGON_H
