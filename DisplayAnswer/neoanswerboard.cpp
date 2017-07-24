09105fa03519c9c64d9dc4a0eb83fda39fd5bca9
shibh308 <s17097@tokyo.kosen-ac.jp>
2017-07-24 15:43:05 +0900

    とりあえずgetPositionつくる

--- DisplayAnswer/neoanswerboard.cpp
+++ DisplayAnswer/neoanswerboard.cpp
@@ -34,14 +34,16 @@ void NeoAnswerBoard::paintEvent(QPaintEvent *event)
     painter.setBrush(QBrush(QColor(back_ground_color)));
     painter.drawRect(QRect(0,0,window_width,window_height));
 
-        //draw grid
+
+
+    //draw grid
     auto drawGrid = [&]{
-        const int grid_size =
+        grid_size =
                 window_width <= window_height
                 ? window_width / (grid_col + grid_margin)
                 : window_height / (grid_row + grid_margin);
-        const int top_buttom_margin = (window_height - grid_size * grid_row) / 2;
-        const int left_right_margin = (window_width - grid_size * grid_col) / 2;
+        top_buttom_margin = (window_height - grid_size * grid_row) / 2;
+        left_right_margin = (window_width - grid_size * grid_col) / 2;
 
         for (int current_col = 0; current_col < grid_col + 1; ++current_col) {
             int x = current_col * grid_size + left_right_margin;
@@ -52,5 +54,25 @@ void NeoAnswerBoard::paintEvent(QPaintEvent *event)
             painter.drawLine(left_right_margin,y,window_width - left_right_margin,y);
         }
     };
+    auto setField = [&]{
+        framepolygon.outer().push_back(point_i(5,5));
+        framepolygon.outer().push_back(point_i(85,7));
+        framepolygon.outer().push_back(point_i(75,48));
+        framepolygon.outer().push_back(point_i(18,57));
+        framepolygon.outer().push_back(point_i(5,5));
+    };
+    auto drawFrame = [&]{
+        painter.setBrush(QBrush(QColor(QString("#00FF00")))); //set color
+        QPointF points[4];
+        for(int tes = 0;tes < 4; tes++){
+            points[tes] = getPosition(framepolygon.outer().at(tes));
+        }
+        painter.drawPolygon(points,4);
+    };
+    setField();
     drawGrid();
+    drawFrame();
+}
+QPointF NeoAnswerBoard::getPosition(point_i point){
+    return QPointF(point.x(),point.y());
 }
--- DisplayAnswer/neoanswerboard.h
+++ DisplayAnswer/neoanswerboard.h
@@ -2,6 +2,10 @@
 #define NEOANSWERBOARD_H
 
 #include <QWidget>
+#include <vector>
+
+#include "field.h"
+#include "neoexpandedpolygon.h"
 
 namespace Ui {
 class NeoAnswerBoard;
@@ -17,6 +21,14 @@ public:
 
 private:
     Ui::NeoAnswerBoard *ui;
+    std::vector<polygon_i> piecepolygon;
+    polygon_i framepolygon;
+    QPointF getPosition(point_i point);
+    int grid_size;
+    int top_buttom_margin;
+    int left_right_margin;
+
+    //Field field;
 
 protected:
     void paintEvent(QPaintEvent *event);
