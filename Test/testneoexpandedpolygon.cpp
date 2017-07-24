6dba5aecd0b23ce72e74d59742850fcc0de10fff
sanoto <kmn2wei@yahoo.co.jp>
2017-07-24 16:50:41 +0900

    テスト用関数作成途中

---
+++ Test/testneoexpandedpolygon.cpp
@@ -0,0 +1,6 @@
+#include "testneoexpandedpolygon.h"
+
+TestNeoExpandedPolygon::TestNeoExpandedPolygon()
+{
+
+}
---
+++ Test/testneoexpandedpolygon.h
@@ -0,0 +1,11 @@
+#ifndef TESTNEOEXPANDEDPOLYGON_H
+#define TESTNEOEXPANDEDPOLYGON_H
+
+
+class TestNeoExpandedPolygon
+{
+public:
+    TestNeoExpandedPolygon();
+};
+
+#endif // TESTNEOEXPANDEDPOLYGON_H
