3a194fb8c549e5ba0e5849a17a3979b8f9f483b2
taki-d <taka410199@gmail.com>
2017-07-27 12:06:01 +0900

    [add]どこからデータを持ってくるか選択できるようにした

--- Kunugida/kunugida.cpp
+++ Kunugida/kunugida.cpp
@@ -1,12 +1,14 @@
 #include "kunugida.h"
 #include "ui_kunugida.h"
 #include "qrlibrary.h"
+#include "neofield.h"
 
 #include <iostream>
 
 #include <QDebug>
 #include <QPushButton>
 #include <QCheckBox>
+#include <QRadioButton>
 
 Kunugida::Kunugida(QWidget *parent) :
     QMainWindow(parent),
@@ -30,10 +32,31 @@ Kunugida::~Kunugida()
 
 void Kunugida::run()
 {
-    std::cout << "Run" << std::endl;
     logger->info("Run Button Clicked");
-    QRLibrary lib;
-    lib.Decoder(true);
+
+    procon::NeoField field;
+
+    if(ui->probmaker_button->isChecked()){
+        //selected probmaker
+        logger->info("Selected ProbMaker DataSource");
+
+    }else if(ui->scanner_button->isChecked()){
+        //selected scanner
+        logger->info("Selected Scanner DataSource");
+
+    }else if(ui->image_data_button->isChecked()){
+        //selected image
+        logger->info("Selected ImageData DataSource");
+
+    }
+//    TODO: ここまでで各データソースから読み込むようにする
+
+
+
+
+//    QRLibrary lib;
+//    lib.Decoder(true);
+    
     this->finishedProcess();
 }
 
