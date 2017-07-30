7f9539fe116aa50008ca83c22dc05af46d20e89d
reikasa <reiichi010509@gmail.com>
2017-07-30 13:12:26 +0900

    [fix] now NeoAnswerDock can use

--- DisplayAnswer/neoanswerdock.cpp
+++ DisplayAnswer/neoanswerdock.cpp
@@ -6,9 +6,29 @@ NeoAnswerDock::NeoAnswerDock(QWidget *parent) :
     ui(new Ui::NeoAnswerDock)
 {
     ui->setupUi(this);
+    // Background image
+    QImage image("../../procon2017-comp/background.png");
+    QGraphicsScene* scene = new QGraphicsScene();
+    QGraphicsPixmapItem* pic = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+    scene->addItem(pic);
+    ui->Picture->setScene(scene);
 }
 
 NeoAnswerDock::~NeoAnswerDock()
 {
     delete ui;
+}
+
+void NeoAnswerDock::addAnswer(const procon::Field &field)
+{
+    fields.push_back(field);
+    AnswerBoard* answer_board = new AnswerBoard(fields.size()-1, true);
+    answer_board->setField(field);
+    answer_board->setFixedSize(300,300);
+    connect(answer_board,&AnswerBoard::clicked_with_id,this,&NeoAnswerDock::clickedAnswer);
+}
+
+void NeoAnswerDock::clickedAnswer(int id)
+{
+    emit selectField(fields.at(id));
 }
--- DisplayAnswer/neoanswerdock.h
+++ DisplayAnswer/neoanswerdock.h
@@ -2,6 +2,15 @@
 #define NEOANSWERDOCK_H
 
 #include <QWidget>
+#include <QScrollArea>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsRectItem>
+#include "field.h"
+#include "utilities.h"
+#include "answerboard.h"
+
 
 namespace Ui {
 class NeoAnswerDock;
@@ -14,9 +23,15 @@ class NeoAnswerDock : public QWidget
 public:
     explicit NeoAnswerDock(QWidget *parent = 0);
     ~NeoAnswerDock();
+    void addAnswer(procon::Field const& field);
 
 private:
     Ui::NeoAnswerDock *ui;
+    std::vector<procon::Field> fields;
+private slots:
+    void clickedAnswer(int id);
+signals:
+    void selectField(procon::Field field);
 };
 
 #endif // NEOANSWERDOCK_H
