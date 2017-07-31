6b3b6dfd250adfdac22b53a79e22c60cf9f506ab
taki-d <taka410199@gmail.com>
2017-07-31 21:46:02 +0900

    [add]add debug mode

--- NeoSolver/Algorithm/beamsearch.cpp
+++ NeoSolver/Algorithm/beamsearch.cpp
@@ -1,5 +1,7 @@
 #include "beamsearch.h"
 
+#define DEBUG_MODE
+
 BeamSearch::BeamSearch()
 {
 
@@ -7,7 +9,11 @@ BeamSearch::BeamSearch()
 
 void BeamSearch::init()
 {
-
+#ifdef DEBUG_MODE
+    std::cout << "DEBUG MODE" << std::endl;
+#else
+    std::cout << "EFFICIENT MODE" << std::endl;
+#endif
 }
 
 void BeamSearch::run(procon::NeoField field){
