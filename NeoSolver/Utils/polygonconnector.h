07fddddf1f6f57801f5b41b5d03ecc69cf31c7d2
taki-d <taka410199@gmail.com>
2017-08-02 11:36:06 +0900

    [add]add connect

--- NeoSolver/Utils/polygonconnector.h
+++ NeoSolver/Utils/polygonconnector.h
@@ -1,6 +1,18 @@
 #ifndef POLYGONCONNECTOR_H
 #define POLYGONCONNECTOR_H
 
+class Connect
+{
+public:
+    //接するべき辺の若い方のpointのindex
+    int frame_side_num = -1;
+    int polygon_side_num = -1;
+
+    //触れてるべき頂点のindex
+    int frame_point_num = -1;
+    int polygon_point_num = -1;
+
+};
 
 class PolygonConnector
 {
