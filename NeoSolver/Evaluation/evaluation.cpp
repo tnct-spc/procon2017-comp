21d8a8a83fb8f1f161980c8cb75ef4b1a2213e53
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-08-02 16:50:49 +0900

    [fix]frameとpolygonで角の隙間が生まれるときの処理

--- NeoSolver/Evaluation/evaluation.cpp
+++ NeoSolver/Evaluation/evaluation.cpp
@@ -7,59 +7,69 @@ Evaluation::Evaluation()
 }
 
 
-std::vector<std::tuple<int , int , int >> Evaluation::evaluation(procon::NeoExpandedPolygon const& frame , procon::NeoExpandedPolygon const& polygon)
-{
-    //それぞれのNEPの頂点を引数として渡し、評価値を返す
-    auto main_evaluation=[&frame , &polygon](int frame_point_index , int polygon_point_index){
-//        //角について評価
-//        auto about_angle = [&frame , &polygon , frame_point_index , polygon_point_index](){
-//            double frame_angle = frame.getSideAngle().at(frame_point_index);
-//            double polygon_angle = polygon.getSideAngle().at(polygon_point_index);
-//            if(frame_angle == polygon_angle){
-//                //フレームとポリゴンの角がちょうどあっているとき
-//                return 1;
-//            }else if(frame_angle > polygon_angle){
-//                //角に隙間があるとき
-//                return -1;
-//            }else if(frame_angle < polygon_angle){
-//                //ポリゴンの角がフレームの角より大きくてありえんとき
-//                return -2;
-//            }
-//        };
-        //辺について評価
-        auto about_length = [&frame , &polygon , frame_point_index , polygon_point_index](){
-            int a = frame_point_index - 1;
-            if(a == -1) a = frame.getSize() - 1;
-            double frame_length1 = frame.getSideLength().at(a);
-            double field_length2 = frame.getSideLength().at(frame_point_index);
-
-            a = polygon_point_index - 1;
-            if(a == -1) a = polygon.getSize() - 1;
-            double polygon_length1 = polygon.getSideLength().at(a);
-            double polygon_length2 = polygon.getSideLength().at(polygon_point_index);
-
-            bool b = frame_length1 == polygon_length1;
-            bool c = field_length2 == polygon_length2;
-            bool d = frame_length1 == polygon_length2;
-            bool e = field_length2 == polygon_length1;
-            if((b && c) || (d && e)){
-                //2辺ちょうどあっている
-                return 2;
-            }else if(b || c || d || e){
-                //1辺どちらかがあっている
-                return 1;
-            }else{
-                return 0;
-            }
+std::vector<std::tuple<int , int , int , int , int>> Evaluation::evaluation(procon::NeoExpandedPolygon const& frame , procon::NeoExpandedPolygon const& polygon)
+{    
+    //隙間があるとき評価値の集まりを返す
+    auto gap_evaluation=[&frame , &polygon](int frame_point_index , int polygon_point_index){
+        //NEPの要素数をマイナス1するだけのらむだ
+        auto minus_one = [](procon::NeoExpandedPolygon nep , int index){
+            index =index - 1;
+            if(index == -1) index = nep.getSize();
+            return index;
+        };
+        //
+        auto length = [&frame , &polygon](int frame_index , int polygon_index){
+            double frame_length = frame.getSideLength().at(frame_index);
+            double polygon_length = polygon.getSideLength().at(polygon_index);
+            if(frame_length == polygon_length) return 1;
+            else return 0;
         };
 
-        //上の結果を集計する
-        int const angle_weight = 1;
-        int const length_weight = 1;
-        int point = 0;
-//        point = point + about_angle() * angle_weight;
-        point = point + about_length() * length_weight;
-        return point;
+        int frame_side_index1 = minus_one(frame , frame_point_index);
+        int frame_side_index2 = frame_point_index;
+        int polygon_side_index1 = minus_one(polygon , polygon_point_index);
+        int polygon_side_index2 = polygon_point_index;
+
+        //それぞれ評価値 , frame_side_index , polygon_side_index , frame_point_index , polygon_point_index
+        typedef std::tuple<int , int , int , int , int> my_tuple;
+        std::vector<my_tuple> vector;
+        vector.push_back(
+                    my_tuple(
+                        length(frame_side_index1 , polygon_side_index1),
+                        frame_side_index1,
+                        polygon_side_index1,
+                        frame_point_index,
+                        polygon_point_index
+                    )
+        );
+        vector.push_back(
+                    my_tuple(
+                        length(frame_side_index1 , polygon_side_index2),
+                        frame_side_index1,
+                        polygon_side_index2,
+                        frame_point_index,
+                        polygon_point_index
+                    )
+        );
+        vector.push_back(
+                    my_tuple(
+                        length(frame_side_index2 , polygon_side_index1),
+                        frame_side_index2,
+                        polygon_side_index1,
+                        frame_point_index,
+                        polygon_point_index
+                    )
+        );
+        vector.push_back(
+                    my_tuple(
+                        length(frame_side_index2 , polygon_side_index2),
+                        frame_side_index2,
+                        polygon_side_index2,
+                        frame_point_index,
+                        polygon_point_index
+                    )
+        );
+        return vector;
     };
 
     //角の状態を返す
@@ -75,20 +85,19 @@ std::vector<std::tuple<int , int , int >> Evaluation::evaluation(procon::NeoExpa
 
     };
 
-    //それぞれ評価値、フレームのインデックス、ポリゴンのインデックス
-    int evaluation , frame_point_index , polygon_point_index;
-    std::vector<std::tuple<int , int , int>> vector;
+    int frame_point_index , polygon_point_index;
+    std::vector<std::tuple<int , int , int , int , int>> vector;
     for(frame_point_index = 0 ; frame_point_index < frame.getSize() ; frame_point_index++){
         for(polygon_point_index = 0 ; polygon_point_index < polygon.getSize() ; polygon_point_index++){
             if(angle_status(frame_point_index , polygon_point_index) == 1){
                 //フレームとポリゴンの角がちょうどあっているとき
             }else if(angle_status(frame_point_index , polygon_point_index) == 0){
                 //角に隙間があるとき
+                std::vector<std::tuple<int , int  , int , int , int>> v = gap_evaluation(frame_point_index , polygon_point_index);
+                vector.insert(vector.end() , v.begin() , v.end());
             }else if(angle_status(frame_point_index , polygon_point_index) == -1){
                 //ポリゴンの角がフレームの角より大きくてありえんとき
             }
-            evaluation = main_evaluation(frame_point_index , polygon_point_index);
-            vector.push_back(std::tuple<int , int , int>(evaluation , frame_point_index , polygon_point_index));
         }
     }
 
