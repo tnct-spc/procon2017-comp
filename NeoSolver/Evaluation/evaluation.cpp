fe851d02052b303d478c370beeaf093d733a686d
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-08-03 11:13:09 +0900

    [add]角の大きさがあり得んときとぴったりのときの処理をまとめた

--- NeoSolver/Evaluation/evaluation.cpp
+++ NeoSolver/Evaluation/evaluation.cpp
@@ -69,35 +69,49 @@ std::vector<my_tuple> Evaluation::evaluation(procon::NeoExpandedPolygon const& f
         return vector;
     };
 
+    //角があったときとありえんときの処理
+    auto default_evaluation = [&minus_one , &length , &frame , &polygon](int angle_evaluation , int frame_point_index , int polygon_point_index){
+        int frame_side_index = minus_one(frame , frame_point_index);
+        int polygon_side_index = minus_one(polygon , polygon_point_index);
+
+        int length_evaluation;
+        if(angle_evaluation == -1){
+            //ありえんときの辺の長さの評価
+            length_evaluation = -1;
+        }else{
+            //角がぴったりだったときの辺の長さの評価
+            length_evaluation =
+                    length(frame_side_index , polygon_side_index)
+                    +
+                    length(frame_point_index , polygon_point_index);
+        }
+
+        const int angle_weight = 1;
+        const int length_weight = 1;
+        int evaluation = angle_evaluation * angle_weight + length_evaluation * length_weight;
+
+        return my_tuple(evaluation,
+                        frame_side_index,
+                        polygon_side_index,
+                        frame_point_index,
+                        polygon_point_index
+                        );
+    };
+
     int frame_point_index , polygon_point_index;
     std::vector<my_tuple> vector;
     for(frame_point_index = 0 ; frame_point_index < frame.getSize() ; frame_point_index++){
         for(polygon_point_index = 0 ; polygon_point_index < polygon.getSize() ; polygon_point_index++){
             if(angle_status(frame_point_index , polygon_point_index) == 1){
                 //フレームとポリゴンの角がちょうどあっているとき
-                int frame_side_index = minus_one(frame , frame_point_index);
-                int point_side_index = minus_one(polygon , polygon_point_index);
-                vector.push_back(my_tuple(length(frame_side_index , point_side_index)
-                                          + length(frame_point_index , polygon_point_index),
-                                          minus_one(frame , frame_point_index),
-                                          minus_one(polygon , polygon_point_index),
-                                          frame_point_index,
-                                          polygon_point_index
-                                          )
-                                 );
+                vector.push_back(default_evaluation(1 , frame_point_index , polygon_point_index));
             }else if(angle_status(frame_point_index , polygon_point_index) == 0){
                 //角に隙間があるとき
                 std::vector<my_tuple> v = gap_evaluation(frame_point_index , polygon_point_index);
                 vector.insert(vector.end() , v.begin() , v.end());
             }else if(angle_status(frame_point_index , polygon_point_index) == -1){
                 //ポリゴンの角がフレームの角より大きくてありえんとき
-                vector.push_back(my_tuple(-1,
-                                          minus_one(frame , frame_point_index),
-                                          minus_one(polygon , polygon_point_index),
-                                          frame_point_index,
-                                          polygon_point_index
-                                          )
-                                 );
+                vector.push_back(default_evaluation(-1 , frame_point_index , polygon_point_index));
             }
         }
     }
