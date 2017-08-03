95155bcdc5052504e9c5e0d7847104a4f935e445
taki-d <taka410199@gmail.com>
2017-08-03 20:28:03 +0900

    [add]いつも気になるsortのアレをすぐ確認できるように書いておいた

--- AbilityCheck/main.cpp
+++ AbilityCheck/main.cpp
@@ -24,5 +24,15 @@ int main(int argc, char *argv[])
 
     std::cout << "disjoint 競合判定時間" << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << std::endl;
 
+    std::vector<int> hoge = { 1,5,3,8,10,19,5,1 };
+
+    for(auto const& v : hoge) std::cout << v << ",";
+    std::cout << std::endl;
+
+    std::sort(hoge.begin(),hoge.end(),[](int r,int l){
+        return r > l;
+    });
+
+
     return 0;
 }
