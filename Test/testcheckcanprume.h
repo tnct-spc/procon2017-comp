ae7650db5818845b24543a959a78851c3a7b480b
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-08-21 12:04:28 +0900

    [add]ピースを全部手打ち

--- Test/testcheckcanprume.h
+++ Test/testcheckcanprume.h
@@ -1,11 +1,13 @@
 #ifndef TESTCHECKCANPRUME_H
 #define TESTCHECKCANPRUME_H
 #include "testerwraper.h"
+#include "neofield.h"
 
 class TestCheckCanPrume : public TesterWraper
 {
 public:
     TestCheckCanPrume();
+    procon::NeoField makeField();
     bool run();
 };
 
