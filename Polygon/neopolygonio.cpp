fb703aecef9ccf3b09bae7786d80456dce927919
taki-d <taka410199@gmail.com>
2017-08-31 23:11:11 +0900

    [add]add NeoPolygonIO class

---
+++ Polygon/neopolygonio.cpp
@@ -0,0 +1,6 @@
+#include "neopolygonio.h"
+
+NeoPolygonIO::NeoPolygonIO()
+{
+
+}
---
+++ Polygon/neopolygonio.h
@@ -0,0 +1,11 @@
+#ifndef NEOPOLYGONIO_H
+#define NEOPOLYGONIO_H
+
+
+class NeoPolygonIO
+{
+public:
+    NeoPolygonIO();
+};
+
+#endif // NEOPOLYGONIO_H
