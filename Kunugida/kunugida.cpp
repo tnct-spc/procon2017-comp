08147099a6104d1e658a3529633ce3a0d966cf9d
Heppoko <yuhki.yasuda@gmail.com>
2017-09-01 15:29:16 +0900

    [update]for now

--- Kunugida/kunugida.cpp
+++ Kunugida/kunugida.cpp
@@ -122,7 +122,7 @@ void Kunugida::clickedRunButton()
 void Kunugida::emitAnswer(procon::NeoField field)
 {
    logger->info("emitted answer");
-   this->board->setField(field);
+   this->board->setField(NeoPolygonIO::importField("../../procon2017-comp/field.csv"));
 }
 
 void Kunugida::finishedProcess()
