11d1fb6b4690207366c8a83cdf3cab43c8a960ed
Heppoko <yuhki.yasuda@gmail.com>
2017-09-02 17:53:25 +0900

    [update]add import '6'

--- Polygon/neopolygonio.cpp
+++ Polygon/neopolygonio.cpp
@@ -26,6 +26,7 @@ NeoPolygonIO::NeoPolygonIO()
  *
  * これに加えてstd::array<bool,50> is_placedが欲しいので
  * 末行にstate:5で{1 or 0}を50個書いて表現することとする
+ * また、state:4はstate,id,x座標,y座標,...の形式になっておる
  */
 
 void NeoPolygonIO::exportPolygon(procon::NeoField field, std::string file_path)
@@ -158,6 +159,35 @@ procon::NeoField NeoPolygonIO::importField(std::string file_path)
             procon::NeoExpandedPolygon polygon(_id);
             polygon.resetPolygonForce(hoge);
             import_field.setPiece(polygon);
+
+        }else if(mode == 6){
+            std::string field_index, frame_index, is_inversed, piece_index, score, frame_point_index, frame_side_index, polygon_point_index, polygon_side_index;
+            bool _is_inversed;
+            std::getline(line_stream, field_index, ',');
+            std::getline(line_stream, frame_index, ',');
+            std::getline(line_stream, is_inversed, ',');
+            std::getline(line_stream, piece_index, ',');
+            std::getline(line_stream, score, ',');
+            std::getline(line_stream, frame_point_index, ',');
+            std::getline(line_stream, frame_side_index, ',');
+            std::getline(line_stream, polygon_point_index, ',');
+            std::getline(line_stream, polygon_side_index, ',');
+            Evaluate ev;
+            ev.fields_index = std::stoi(field_index);
+            ev.frame_index = std::stoi(frame_index);
+            if(is_inversed == "0")
+                _is_inversed = false;
+            if(is_inversed == "1")
+                _is_inversed =true;
+            ev.is_inversed = _is_inversed;
+            ev.piece_index = std::stoi(piece_index);
+            ev.score = std::stod(score);
+            ev.connection.frame_point_index = std::stoi(frame_point_index);
+            ev.connection.frame_side_index = std::stoi(frame_side_index);
+            ev.connection.polygon_point_index = std::stoi(polygon_point_index);
+            ev.connection.polygon_side_index = std::stoi(polygon_side_index);
+            import_field.evaluate_cache.push_back(ev);
+
         }else{
             while(std::getline(line_stream, x, ',')){
                 std::getline(line_stream, y, ',');
