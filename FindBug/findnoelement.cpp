43b2dece0a9bdec9e03af052ea3e13495956f6c4
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-09-02 19:44:08 +0900

    [fix]まともにcsvからfieldをもってこれるようにした

--- FindBug/findnoelement.cpp
+++ FindBug/findnoelement.cpp
@@ -1,5 +1,6 @@
 #include "findnoelement.h"
 #include "neopolygonio.h"
+#include "neopolygonviewer.h"
 
 FindNoElement::FindNoElement()
 {
@@ -7,9 +8,13 @@ FindNoElement::FindNoElement()
 }
 
 bool FindNoElement::run(){
-    procon::NeoField field = NeoPolygonIO::importField("/home/yui/Procon/fieldcdv/");
-    nab = std::make_shared<NeoAnswerBoard>();
-    nab->setField(field);
-    nab->show();
+    procon::NeoField field = NeoPolygonIO::importField("/home/yui/Procon/fieldcdv/made_field5.csv");
+    for(procon::NeoExpandedPolygon i : field.getElementaryPieces()){
+        std::cout<<"-------------------------------"<<std::endl;
+        for(point_i j : i.getPolygon().outer()){
+            std::cout<<"x = "<<j.x()<<" , y = "<<j.y()<<std::endl;
+        }
+        NeoPolygonViewer::getInstance().displayPolygon(i.getPolygon(),"",false);
+    }
     return true;
 }
