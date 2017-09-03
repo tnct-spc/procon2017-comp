24d2cb608735cdbe3136783f3d3c7a4e2f16053d
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-09-03 17:52:57 +0900

    [add]findnoelementのfieldを取るときdialogをつかう

--- FindBug/findnoelement.cpp
+++ FindBug/findnoelement.cpp
@@ -1,6 +1,8 @@
 #include "findnoelement.h"
 #include "neopolygonio.h"
 #include "neopolygonviewer.h"
+#include <QFileDialog>
+#include <QString>
 
 FindNoElement::FindNoElement()
 {
@@ -8,7 +10,10 @@ FindNoElement::FindNoElement()
 }
 
 bool FindNoElement::run(){
-    procon::NeoField field = NeoPolygonIO::importField("/home/yui/Procon/fieldcdv/made_field5.csv");
+
+    std::string pieces_path = QFileDialog::getOpenFileName().toStdString();
+
+    procon::NeoField field = NeoPolygonIO::importField(pieces_path);
     for(procon::NeoExpandedPolygon i : field.getElementaryPieces()){
         std::cout<<"-------------------------------"<<std::endl;
         for(point_i j : i.getPolygon().outer()){
