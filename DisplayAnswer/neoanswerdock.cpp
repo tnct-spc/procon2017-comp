f588baaf2ff013b9aa81a8438d4ef4dba594d24c
taki-d <taka410199@gmail.com>
2017-09-08 17:11:10 +0900

    [fix] now fixing f bug

--- DisplayAnswer/neoanswerdock.cpp
+++ DisplayAnswer/neoanswerdock.cpp
@@ -27,8 +27,12 @@ void NeoAnswerDock::addAnswer(const procon::NeoField &field)
     NeoAnswerBoard* answer_board = new NeoAnswerBoard();
     answer_board->setField(field);
     answer_board->setSingleMode(true);
-    answer_board->setFixedSize(400, 400);
-    this->ui->board_container->addWidget(answer_board, (fields.size() - 1 ) / 3, (fields.size() - 1 ) % 3);
+    answer_board->setFixedSize(300, 300);
+    this->ui->board_container->addWidget(answer_board, (fields.size() - 1 ) / 5, (fields.size() - 1 ) % 5);
+
+    QEventLoop loop;
+    QTimer::singleShot(2, &loop, SLOT(quit()));
+    loop.exec();
 }
 
 //Add single piece
@@ -36,13 +40,14 @@ void NeoAnswerDock::makePieceList(const procon::NeoField &field)
 {
     this->setWindowTitle("Piece List");
     this->ui->gridLayout_3->setSpacing(5);
-    this->show();
+    //this->show();
 
     int i = 0;
 
-    for(auto piece : field.getPieces()){
+    for(auto piece : field.getElementaryPieces()){
        NeoPolygonViewer* piece_list = new NeoPolygonViewer;
        piece_list->getInstance().displayPolygon(piece.getPolygon(), "Piece", true);
+       piece_list->setFixedSize(500, 500);
        this->ui->board_container->addWidget(piece_list, i / 3, i % 3);
        i++;
        this->update();
