86004ed37f131a4dc669b6895b1a0d8d05e25175
sanoto <kmn2wei@yahoo.co.jp>
2017-09-25 12:22:58 +0900

    [add] 関数追加

--- Polygon/Utils/checknearly.cpp
+++ Polygon/Utils/checknearly.cpp
@@ -4,3 +4,13 @@ CheckNearly::CheckNearly()
 {
 
 }
+
+std::vector<procon::ExpandedPolygon> CheckNearly::fit_nearly(std::vector<procon::ExpandedPolygon> expolys_d_in, std::vector<procon::NeoExpandedPolygon> expolys_i_in)
+{
+
+}
+
+double CheckNearly::check_nearly(procon::ExpandedPolygon expoly_d_in, procon::NeoExpandedPolygon expoly_i_in)
+{
+
+}
--- Polygon/Utils/checknearly.h
+++ Polygon/Utils/checknearly.h
@@ -1,11 +1,15 @@
 #ifndef CHECKNEARLY_H
 #define CHECKNEARLY_H
 
+#include "expandedpolygon.h"
+#include "neoexpandedpolygon.h"
 
 class CheckNearly
 {
 public:
     CheckNearly();
+    static double check_nearly(procon::ExpandedPolygon expoly_d_in, procon::NeoExpandedPolygon expoly_i_in);
+    static std::vector<procon::ExpandedPolygon> fit_nearly(std::vector<procon::ExpandedPolygon> expolys_d_in, std::vector<procon::NeoExpandedPolygon> expolys_i_in);
 };
 
 #endif // CHECKNEARLY_H
