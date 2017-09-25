23d74831be01a612e16e1f46c2e281f20e52fb1d
shibh308 <s17097@tokyo.kosen-ac.jp>
2017-09-25 13:06:07 +0900

    [add]add testQrTranslateToPolygon

---
+++ Test/testqrtranslatetopolygon.h
@@ -0,0 +1,11 @@
+#ifndef TESTQRTRANSLATETOPOLYGON_H
+#define TESTQRTRANSLATETOPOLYGON_H
+#include "../QRcode/qrtranslatetopolygon.h"
+#include "testerwraper.h"
+class TestQrTranslateToPolygon : public TesterWraper
+{
+public:
+    TestQrTranslateToPolygon();
+};
+
+#endif // TESTQRTRANSLATETOPOLYGON_H
