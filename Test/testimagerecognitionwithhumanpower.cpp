ccfdbad2a9048b31bb5f6f57368b914e42235af0
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-09-28 18:18:18 +0900

    [fix]testのpolygonをdouble値に変更

--- Test/testimagerecognitionwithhumanpower.cpp
+++ Test/testimagerecognitionwithhumanpower.cpp
@@ -9,7 +9,7 @@ TestImageRecognitionWithHumanPower::TestImageRecognitionWithHumanPower()
 bool TestImageRecognitionWithHumanPower::run()
 {
     polygon_t polygon;
-    bg::exterior_ring(polygon) = boost::assign::list_of<point_t>(0, 0)(0, 3)(3, 3)(0, 0);
+    bg::exterior_ring(polygon) = boost::assign::list_of<point_t>(0.5, 0.5)(0.1, 3.0)(3.0, 3.1)(0.5, 0.5);
 
     imagerecongnitionwithhumanpower *iwh = new imagerecongnitionwithhumanpower();
     iwh->setPolygon(polygon);
