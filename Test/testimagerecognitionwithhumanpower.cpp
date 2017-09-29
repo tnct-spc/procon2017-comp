bc40d35568623470ebb983179eae40113a1754a7
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-09-29 15:17:47 +0900

    [fix]testのupdateがなくても動くようにした

--- Test/testimagerecognitionwithhumanpower.cpp
+++ Test/testimagerecognitionwithhumanpower.cpp
@@ -17,7 +17,7 @@ bool TestImageRecognitionWithHumanPower::run()
     imagerecongnitionwithhumanpower *iwh = new imagerecongnitionwithhumanpower();
     iwh->setPolygon(polygon);
     iwh->show();
-    iwh->update();
+//    iwh->update();
     std::cout<<"hello"<<std::endl;
     return 1;
 }
