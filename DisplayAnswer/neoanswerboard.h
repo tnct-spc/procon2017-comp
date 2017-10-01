86a55e19f48664abc1f312f5894e3bbf8c21c67f
shibh308 <s17097@tokyo.kosen-ac.jp>
2017-10-01 18:15:56 +0900

    [add]showunplacedpieceの雛形、画面右側の余白

--- DisplayAnswer/neoanswerboard.h
+++ DisplayAnswer/neoanswerboard.h
@@ -38,6 +38,8 @@ public:
     void setUp();
     void setSelectPieceMode(bool mode);
 
+    void setShowUnplacedPieces(bool input);
+
     std::vector<int> getSelectedPieceId();
 
 private:
@@ -59,11 +61,12 @@ private:
     procon::NeoField field;
 
     // Only field mode
-    bool single_mode = false;
+    bool single_mode = true;//false
     bool singleif = false;
     int piece_size = 1;
     bool allif = true;
     bool select_piece_mode = false;
+    bool showunplacedpieces = true;//false
 
     //make id_list
     std::vector<polygon_i> polygon_list;
