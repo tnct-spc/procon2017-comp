14ee81b34b1a2bc910883c81a326deca033bed1e
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-10-01 20:05:38 +0900

    [add]許容範囲の処理を追加

--- ImageRecognition/imagerecongnitionwithhumanpower.cpp
+++ ImageRecognition/imagerecongnitionwithhumanpower.cpp
@@ -78,11 +78,11 @@ QPointF imagerecongnitionwithhumanpower::toWindowPoint(QPointF point){
 }
 
 void imagerecongnitionwithhumanpower::mousePressEvent(QMouseEvent *event){
-    const double threshold = 0.5;
+    const double threshold = ui->doubleSpinBox->value();
     QPointF selected_point = toPolygonPoint(event->x(),event->y());
     std::cout<<selected_point.x()<<","<<selected_point.y()<<std::endl;
     auto itr = std::find_if(points.begin(),points.end(),[&](QPointF p){
-        return (std::fabs(p.x() - selected_point.x()) < threshold) && (std::fabs(p.y() - selected_point.y()) < threshold);
+        return (std::fabs(p.x() - selected_point.x()) <= threshold) && (std::fabs(p.y() - selected_point.y()) <= threshold);
     });
     selecting = itr != points.end();
     if(selecting){
