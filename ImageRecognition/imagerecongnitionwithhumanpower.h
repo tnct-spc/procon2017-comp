9acc8f9e2f647dc54be5e82b5391a16ce2ebe97b
nakanoyoshiharu <nonomuraryutaro1966@gmail.com>
2017-10-02 15:10:02 +0900

    [rm]今までのをすべて消した

--- ImageRecognition/imagerecongnitionwithhumanpower.h
+++ ImageRecognition/imagerecongnitionwithhumanpower.h
@@ -2,14 +2,8 @@
 #define IMAGERECONGNITIONWITHHUMANPOWER_H
 
 #include <QWidget>
-#include <QGraphicsView>
-#include <QGraphicsPixmapItem>
-#include <QLayout>
-#include <QSpacerItem>
-#include <QString>
 #include "precompile.h"
 #include "polygonviewer.h"
-#include "singlepolygondisplay.h"
 
 namespace Ui {
 class imagerecongnitionwithhumanpower;
@@ -23,32 +17,15 @@ public:
     explicit imagerecongnitionwithhumanpower(QWidget *parent = 0);
     ~imagerecongnitionwithhumanpower();
 
-    void setPolygon(polygon_t polygon);
-    void setImage(QString path);
-    void setImage(cv::Mat mat);
-
 private:
-    QPointF toPolygonPoint(double window_x,double window_y);
-    QPointF toWindowPoint(QPointF point);
     Ui::imagerecongnitionwithhumanpower *ui;
-    polygon_t polygon;
-    std::vector<QPointF> points;
-    int grid_size,left_right_margin,top_buttom_margin;
-    std::pair<double,double> minXY;
-    bool selecting;
-    int move_index;
 
 private slots:
-    void clickedEditedButton();
-    void clickedEnlargement();
-    void clickedReduction();
+
 protected:
-    void paintEvent(QPaintEvent *);
-    void mousePressEvent(QMouseEvent *event);
-    void mouseReleaseEvent(QMouseEvent *event);
 
 signals:
-    void returnPolygon(polygon_t polygon);
+
 };
 
 #endif // IMAGERECONGNITIONWITHHUMANPOWER_H
--- Test/testimagerecognitionwithhumanpower.cpp
+++ Test/testimagerecognitionwithhumanpower.cpp
@@ -3,7 +3,6 @@
 
 #include <QObject>
 
-
 TestImageRecognitionWithHumanPower::TestImageRecognitionWithHumanPower()
 {
 
@@ -15,21 +14,7 @@ bool TestImageRecognitionWithHumanPower::run()
     bg::exterior_ring(polygon) = boost::assign::list_of<point_t>(0.5, 0.5)(0.1, 3.0)(3.0, 3.1)(0.5, 0.5);
     std::cout << "polygon" << bg::dsv(polygon) << std::endl;
 
-    cv::Mat image = cv::imread("../../../Pictures/k-on.png");
-
-    imagerecongnitionwithhumanpower *iwh = new imagerecongnitionwithhumanpower();
-
-    QObject::connect(iwh,&imagerecongnitionwithhumanpower::returnPolygon,[](polygon_t returnpolygon){
-           std::cout << "called" << std::endl;
-
-           std::cout << boost::geometry::dsv(returnpolygon) << std::endl;
-    });
-
-    iwh->setPolygon(polygon);
-//    iwh->setImage("../../procon2017-comp/background.png");
-    iwh->setImage(image);
-    iwh->show();
-//    iwh->setPolygon(polygon);
-    std::cout<<"hello"<<std::endl;
+    imagerecongnitionwithhumanpower *irwh = new imagerecongnitionwithhumanpower();
+    irwh->show();
     return 1;
 }
